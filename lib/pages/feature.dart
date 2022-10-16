e3812bd7c402016134d5dec3722d1aab2bedc730
farisqadr <faris.code@gmail.com>
2022-10-16 17:26:12 +0700

    stateful widget day 6 alhamdulillah

--- lib/pages/feature.dart
+++ lib/pages/feature.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sevendays/widget/theme.dart';
 
-class FeatureLight extends StatelessWidget {
+class FeatureLight extends StatefulWidget {
+  @override
+  State<FeatureLight> createState() => _FeatureLightState();
+}
+
+class _FeatureLightState extends State<FeatureLight> {
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     Widget header() {
@@ -27,50 +33,75 @@ class FeatureLight extends StatelessWidget {
       );
     }
 
-    Widget option() {
-      return Container(
-        width: 300,
-        height: 80,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(60),
-            border: Border.all(color: Color(0xffD9DEEA))),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 25, top: 15, bottom: 15),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/pig.png',
-                width: 50,
-              ),
-              SizedBox(width: 5),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 3),
-                    child: Text('Money Security',
-                        style: GoogleFonts.poppins(
-                            fontSize: 16, fontWeight: FontWeight.w500)),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'support ',
-                        style: lightTextStyle,
-                      ),
-                      Text(
-                        '24/7',
-                        style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          color: Color(0xff5343C2),
-                          fontWeight: FontWeight.w500,
+    Widget option(
+      int index,
+      String imageUrl,
+      String title,
+      String description,
+      String subdescription,
+    ) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Container(
+          width: 300,
+          height: 80,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(60),
+              border: Border.all(
+                  color: selectedIndex == index
+                      ? Color(0xff6050E7)
+                      : Color(0xffD9DEEA))),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25, top: 15, bottom: 15),
+            child: Row(
+              children: [
+                Image.asset(
+                  imageUrl,
+                  width: 50,
+                ),
+                SizedBox(width: 5),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 3),
+                      child: Text(title,
+                          style: GoogleFonts.poppins(
+                              fontSize: 16, fontWeight: FontWeight.w500)),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          description,
+                          style: lightTextStyle,
                         ),
+                        Text(
+                          subdescription,
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            color: Color(0xff5343C2),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                selectedIndex == index
+                    ? Image.asset(
+                        'assets/icon_check_purple.png',
+                        width: 25,
                       )
-                    ],
-                  )
-                ],
-              )
-            ],
+                    : SizedBox()
+              ],
+            ),
           ),
         ),
       );
@@ -83,7 +114,20 @@ class FeatureLight extends StatelessWidget {
         left: 30,
       ),
       child: Column(
-        children: [header(), option()],
+        children: [
+          header(),
+          option(0, 'assets/pig.png', 'Money Security', 'support ', '24/7'),
+          SizedBox(
+            height: 14,
+          ),
+          option(1, 'assets/small_doc.png', 'Automation', 'we provide ',
+              'invoice'),
+          SizedBox(
+            height: 14,
+          ),
+          option(
+              2, 'assets/dollar.png', 'Business Report', 'can up to ', '10K'),
+        ],
       ),
     ));
   }
